6c105e129f81c915ce48c10daa08db64b77cbff9
winay05 <vinay.manipal05@gmail.com>
2021-07-18 04:23:50 +0530

    Implemented editable columns

--- src/components/Table/TableRow.jsx
+++ src/components/Table/TableRow.jsx
@@ -1,12 +1,27 @@
+import { useState } from "react";
 import "./Table.css";
 
 const TableRow = (props) => {
+  const [editing, toggleEdit] = useState(false);
+
   const handleDelete = (id) => {
     props.delete(id);
   };
   const handleToggle = (id) => {
     console.log("inside click handler");
     props.onSelect(id);
+  };
+  const handleSave = () => {
+    toggleEdit(false);
+    document.getElementById("name").disabled = true;
+    document.getElementById("email").disabled = true;
+    document.getElementById("role").disabled = true;
+  };
+  const handleEdit = () => {
+    toggleEdit(true);
+    document.getElementById("name").disabled = false;
+    document.getElementById("email").disabled = false;
+    document.getElementById("role").disabled = false;
   };
 
   return (
@@ -21,11 +36,29 @@ const TableRow = (props) => {
           value={props.el.id}
         />
       </td>
-      <td id="name">{props.el.name}</td>
-      <td id="email">{props.el.email}</td>
-      <td id="role">{props.el.role}</td>
+      <td>
+        <input id="name" type="text" defaultValue={props.el.name} disabled />
+      </td>
+      <td>
+        <input id="email" type="email" defaultValue={props.el.email} disabled />
+      </td>
+      <td>
+        <select id="role" defaultValue={props.el.role} disabled>
+          <option value="member">Member</option>
+          <option value="admin">Admin</option>
+          <option value="guest">Guest</option>
+        </select>
+      </td>
       <td id="actions">
-        <button>
+        {editing ? (
+          <button onClick={handleSave}>
+            <img
+              src="https://img.icons8.com/ios-glyphs/30/000000/save--v2.png"
+              alt="save button"
+            />
+          </button>
+        ) : null}
+        <button onClick={handleEdit}>
           <img
             className="icon"
             src="https://img.icons8.com/material-outlined/24/000000/edit--v1.png"
